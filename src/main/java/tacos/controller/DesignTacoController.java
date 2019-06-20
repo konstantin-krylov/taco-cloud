a7b1c70ca33d2a7f12dc3d42009c02863389d90c
Konstantin <kos.krylov@mail.ru>
2019-06-20 21:24:51 +0300

    Initial commit

---
+++ src/main/java/tacos/controller/DesignTacoController.java
@@ -0,0 +1,80 @@
+package tacos.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import tacos.model.Ingredient;
+import tacos.model.Taco;
+import tacos.model.Type;
+
+import javax.validation.Valid;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RequestMapping("/design")
+@Controller
+public class DesignTacoController {
+
+    @GetMapping
+    public String showDesignForm(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+                new Ingredient("CHED", "Cheddar", Type.CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+                new Ingredient("SLSA", "Salsa", Type.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
+        );
+
+        Type[] types = Type.values();
+
+        for (Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(),
+                    filterByType(ingredients, type));
+
+            model.addAttribute("design", new Taco());
+
+        }
+        return "design"; // view that will be shown to the user
+    }
+
+//    @PostMapping
+//    public String processDesign(Design design) {
+//        log.info("Processing design: " + design);
+//
+//        //after processDesign() completes, the user’s browser should be redirected to the relative path /order/current.
+//        return "redirect:/orders/current";
+//
+//    }
+
+    @PostMapping
+    public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+
+        log.info("Processing design: " + design);
+
+        return "redirect:/orders/current";
+    }
+
+
+    private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+        return ingredients.stream()
+                .filter(x -> x.getType().equals(type))
+                .collect(Collectors.toList());
+    }
+
+
+}
---
+++ src/main/java/tacos/model/Type.java
@@ -0,0 +1,5 @@
+package tacos.model;
+
+public enum Type {
+    WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+}
